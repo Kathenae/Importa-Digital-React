30b1945f3fdcd7883b3fbe364959626dc72d2d40
Ronald <RonaldWilsonMachado@gmail.com>
2023-11-15 11:24:33 +0200

    feat: Add new dependencies and update tailwind configuration
    This commit adds new dependencies `@egoist/tailwindcss-icons@1.4.0` and `@iconify/json@2.2.139` to the project's `package.json` file.
    
    In the `tailwind.config.js` file, the following changes were made:
    - Imported `iconsPlugin` and `getIconCollections` from `@egoist/tailwindcss-icons`.
    - Updated the `fontFamily.sans` property to use the `'Manderley'` font.
    - Added a new `container` configuration with centered alignment and a padding of `'12rem'`.
    - Added new color configurations for the `'primary'` and `'secondary'` color schemes.
    
    These changes improve the project's functionality by adding new dependencies and customizing the tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -13,10 +15,48 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Manderley', ...defaultTheme.fontFamily.sans],
             },
+            container: {
+                center: true,
+                padding: '12rem',
+            },
+            colors: {
+                'primary': {
+                    '50': '#fff1f2',
+                    '100': '#ffe4e7',
+                    '200': '#fecdd4',
+                    '300': '#fea3b1',
+                    '400': '#fc7087',
+                    '500': '#f53e61',
+                    '600': '#e42251',
+                    '700': '#bf113f',
+                    '800': '#a0113c',
+                    '900': '#89123a',
+                    '950': '#4c051b',
+                },
+                'secondary': {
+                    '50': '#f2f7fd',
+                    '100': '#e5eef9',
+                    '200': '#c5dcf2',
+                    '300': '#92c0e7',
+                    '400': '#589ed8',
+                    '500': '#3282c5',
+                    '600': '#2367a6',
+                    '700': '#1d5287',
+                    '800': '#1c4770',
+                    '900': '#1c3c5e',
+                    '950': '#13273f',
+                },
+
+            }
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        iconsPlugin({
+            collections: getIconCollections(["mdi", "lucide"]),
+        })
+    ],
 };
